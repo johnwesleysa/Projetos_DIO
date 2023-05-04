b7027d1c04d378678dcd0281b5cea58c32fad906
johnwesleysa <john.wesley.alencar2@gmail.com>
2023-05-03 23:03:08 -0300

    Começando
    Começando

--- Primeiras Páginas Interativas com JS/Pokedex/assets/js/main.js
+++ Primeiras Páginas Interativas com JS/Pokedex/assets/js/main.js
@@ -7,18 +7,19 @@ let offset = 0
 function convertPokemonToList (pokemon) {
 
     return `
-    <li class="pokemon ${pokemon.type}" >
-                <span class="number">#${pokemon.number}</span>
-                <span class="name">${pokemon.name}</span>
-                
-                <div class="detail">
-                    <ol  class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}" >${type}</li>`).join('')}
-                    </ol>
-
-                    <img src="${pokemon.photo}" alt="${pokemon.name}">
-                </div>
-            </li>
+    <a href="detail-pokemon.html" style="text-decoration: none;">
+        <li class="pokemon ${pokemon.type}  >
+                    <span class="number">#${pokemon.number}</span>
+                    <span class="name">${pokemon.name}</span>
+        
+                    <div class="detail">
+                        <ol  class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}" >${type}</li>`).join('')}
+                        </ol>
+                        <img src="${pokemon.photo}" alt="${pokemon.name}">
+                    </div>
+                </li>
+    </a>
     `
 }
 
