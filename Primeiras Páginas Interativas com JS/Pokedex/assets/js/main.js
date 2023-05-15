40c8cbc7045d5a135ca4728a4ded33514c53e709
johnwesleysa <john.wesley.alencar2@gmail.com>
2023-05-14 23:42:24 -0300

    terminou
    terminou

--- Primeiras Páginas Interativas com JS/Pokedex/assets/js/main.js
+++ Primeiras Páginas Interativas com JS/Pokedex/assets/js/main.js
@@ -27,78 +27,91 @@ function convertPokemonToLi(pokemon) {// Alterações de identificação do núm
 }
 
 const selectPokemon = async (id) => {
+    const title = document.getElementById('title')
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
     const res = await fetch(url)
     const pokemon = await res.json()
-    displayPopup(pokemon)
+    newPagePokemon(pokemon)
+    loadMoreButton.parentElement.removeChild(loadMoreButton)
+    pokemonList.parentElement.removeChild(pokemonList)
+    title.parentElement.removeChild(title)
 }
   
-const displayPopup = (pokemon) => {
-        /* 
-        Opções de Imagens:
-        svg: pokemon.sprites.other.dream_world.front_default
-        png: pokemon.sprites.other.home.front_default
-        */
+const newPagePokemon = (pokemon) => {
+    const contentPokemonId = document.getElementById('contentPokemonId')
+    
     const imagem = pokemon.sprites.other.dream_world.front_default
     const types = pokemon.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
 
     pokemon.types = types
     pokemon.type = type
-  
-    const htmlString = `
-        <div class="modal">
-            <div id="details">
-                <li class="pokemon-modal ${pokemon.type}">
-                    <span class="number">#0${pokemon.id}</span>
+
+    const detailPokemon = `
+    <div class="contentPokemon ${pokemon.type}">
+        <header class="header">
+            <a href="index.html" class="backButtom">
+                <img  class="backImg"src="assets/images/icons/arrow-back.png" alt="Back">
+            </a>
+            <button class="buttonLike">
+                <img class="likeButton" src="assets/images/icons/like.png" alt="Like">
+            </button>
+        </header>
+        
+            <div class="pokemonInfo">
+                <li class="pokemon ${pokemon.type}">
+                    <span class="number">#${pokemon.id}</span>
                     <span class="name">${pokemon.name}</span>
-
-                        <div class="detail">
-                            <ol class="types">
-                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                            </ol>
-                        </div>
-
-                    <img id="pokemon-img" src="${imagem}" alt="${pokemon.name}">
-
-                    <div id="data">
-                        <h3>Base stats</h3>
-
-                            <div id="stats">
-
-                                <div class="stats-name">
-                                ${pokemon.stats.map((name_stats) => `<p>${name_stats.stat.name}</p>`).join('')}
-                                </div>
-
-                                <div class="stats-description">
-                                ${pokemon.stats.map((base_stats) => `<p>${base_stats.base_stat}</p>`).join('')}
-                                </div>
-                            </div>
-
-                            <div class="stats-body">
-                                <p class="${type}">Altura:${(pokemon.height/10).toFixed(2)} m</p>
-                                <p class="${type}">Peso:${(pokemon.weight/10)} kg</p>
-                            </div>
-                        </div>
+                    <div class="detail">
+                        <ol class="types">
+                            <div class="typeDiv">${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}</div>
+                        </ol>
                     </div>
                 </li>
-                <button class="button cancel" id="cancel" onClick="modalClose()">Fechar</button>
+                <div class="imgPokemon">
+                    <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+                </div>
+            </div>
+        
+        <div class="pokemonStatus">
+            <div class="statusNavegacao">
+                        <div class="infoListPokemon">
+                            <h1>About</h1>
+                        </div>
+                        <div class="aboutPokemon">
+                            <li class="nameInformation">
+                                <ol>
+                                    <li>Species</li>
+                                    <li>Height</li>
+                                    <li>Weight</li>
+                                    
+                                </ol>
+                            </li>
+                            <li class="informationName">
+                                <ol>
+                                    <li style="text-transform: capitalize;">${type}</li>
+                                    <li>${pokemon.height}</li>
+                                    <li>${pokemon.weight}</li>
+                                   
+                                </ol>
+                            </li>
+                        </div>
             </div>
         </div>
-
+        </div>
     `
-    pokemonList.innerHTML = htmlString + pokemonList.innerHTML
+       
+   
+    contentPokemonId.innerHTML = detailPokemon 
 }
 
-const modalClose = () => {
-    const close = document.querySelector('.modal')
-    close.parentElement.removeChild(close)
-}
+
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
+        const listPokemon = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += listPokemon
+        
     })
 }
 
--- Primeiras Páginas Interativas com JS/Pokedex/assets/js/poke-api.js
+++ Primeiras Páginas Interativas com JS/Pokedex/assets/js/poke-api.js
@@ -4,15 +4,12 @@ function convertPokeApiDetailToPokemon(pokeDetail, url) {
     const pokemon = new Pokemon()
     pokemon.name = pokeDetail.name
     pokemon.number = pokeDetail.id
-
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
-
     pokemon.types = types
     pokemon.type = type
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
     pokemon.url = pokeDetail.url
-
     return pokemon
 }
 
